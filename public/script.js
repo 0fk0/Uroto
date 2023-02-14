dee6324a6a80331a4982e9b3bde53b213f700e2b
0fk0 <greeeen_ken41ei39jin@icloud.com>
2023-02-14 23:38:01 +0900

    remove needless files

---
+++ public/script.js
@@ -0,0 +1,39 @@
+class Introduction extends React.Component{
+    goodFace = ":)";
+    badFace = ":(";
+    constructor(props){
+        super(props);
+        this.state = {
+            isToggleOn: true
+        };
+
+        this.CaseByClick = this.CaseByClick.bind(this);
+    }
+
+    CaseByClick(){
+        this.setState(prev => ({
+            isToggleOn: !prev.isToggleOn
+        }));
+    }
+    
+    render(){
+        if (this.state.isToggleOn){
+            return (
+                <div>
+                    <button onClick={this.CaseByClick}>{this.goodFace}</button>
+                    <h1>So Cool!</h1>    
+                </div>
+            );
+        }
+        return (
+            <div>
+                <button onClick={this.CaseByClick}>{this.badFace}</button>
+                <h1>You are stupid!</h1>
+            </div>
+        );
+    }
+}
+
+const dom = document.querySelector("#root");
+const elm = <Introduction/>;
+ReactDOM.render(elm, dom);
