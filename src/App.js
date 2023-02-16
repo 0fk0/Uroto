c7785024a841ad946e8b52d5471f95fcae4d0362
0fk0 <greeeen_ken41ei39jin@icloud.com>
2023-02-16 13:50:19 +0900

    add header component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import Introduction from './components/Introduction';
+import Contents from './components/Contents';
+import Header from './components/Header';
 
 function App(){
   return (
     <div className="App">
+      <Header/>
       <Introduction/>
-      <Introduction/>
-      <Introduction/>
-      <Introduction/>
+      <Contents/>
     </div>
   );
 }
