b5b01624af8fb151b42b204b4b3af543b3caf53d
0fk0 <greeeen_ken41ei39jin@icloud.com>
2023-02-17 12:44:01 +0900

    change height for smartphone

--- src/components/modules_js/setWindowSize.js
+++ src/components/modules_js/setWindowSize.js
@@ -1,6 +1,9 @@
+// アドレスバーを加味して画面全体の割合で高さを決定する
+// ここだけやむを得ずjsにcssが混入
 function setWindowSize(){
     let win_H = window.innerHeight;
-    document.querySelector('body').style.height = win_H;
+    document.querySelector('#Header').style.height = win_H * 0.03;
+    document.querySelector('.content').style.height = win_H * 0.97;
 }
 
 export default setWindowSize;
